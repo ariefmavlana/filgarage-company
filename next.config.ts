80aa46370c3cafb03e3b1aa64ac64a37e22be3ba
ariefmavlana <ariefmaulana0808@gmail.com>
2025-09-11 19:55:58 +0700

    comm 3.5

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   /* config options here */
   experimental: {
     optimizeCss: true, // jika menggunakan Tailwind
